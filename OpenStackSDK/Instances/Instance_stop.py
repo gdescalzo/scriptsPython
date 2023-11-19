404deb3aa9d5dc10ebade65d75f484e7e42fc11c
Gaston Descalzo <gdescalzo@outlook.com>
2023-11-19 10:16:55 -0300

    Enhance the stop module

--- OpenStackSDK/Instances/Instance_stop.py
+++ OpenStackSDK/Instances/Instance_stop.py
@@ -1,21 +1,32 @@
 import openstack
 
+def list_instances(conn):
+    instances = conn.compute.servers()
+    print("Available instances:")
+    for instance in instances:
+        print(f"ID: {instance.id}, Name: {instance.name}")
+
+def stop_instance(conn, instance_name):
+    try:
+        # Get information about the instance by name
+        instance = conn.compute.find_server(instance_name)
+
+        # Check if the instance exists
+        if instance:
+            # Stop the instance
+            conn.compute.stop_server(instance)
+            print(f"Instance '{instance_name}' stopped successfully.")
+        else:
+            print(f"No instance found with the name '{instance_name}'.")
+    except Exception as e:
+        print(f"Error while trying to stop the instance: {e}")
+
 # Initialize the connection
 conn = openstack.connect(cloud='openstack')
 
-# Name of the instance you want to stop
-instance_name = 'test1'
+# List and select an instance to stop
+list_instances(conn)
+instance_name_to_stop = input("Enter the name of the instance to stop: ")
 
-try:
-    # Get information about the instance by name
-    instance = conn.compute.find_server(instance_name)
-
-    # Check if the instance exists
-    if instance:
-        # Stop the instance
-        conn.compute.stop_server(instance)
-        print(f"Instance '{instance_name}' stopped successfully.")
-    else:
-        print(f"No instance found with the name '{instance_name}'.")
-except Exception as e:
-    print(f"Error while trying to stop the instance: {e}")
+# Stop the selected instance
+stop_instance(conn, instance_name_to_stop)
