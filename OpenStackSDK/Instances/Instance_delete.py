c7dfce7709fa79e2f4776cb20a350067d3139273
Gaston Descalzo <gdescalzo@outlook.com>
2023-11-19 10:36:07 -0300

    Enhance the delete module

--- OpenStackSDK/Instances/Instance_delete.py
+++ OpenStackSDK/Instances/Instance_delete.py
@@ -3,26 +3,48 @@ import openstack
 def list_instances(conn):
     instances = conn.compute.servers()
     print("Available instances:")
+    print('')
     for instance in instances:
         print(f"ID: {instance.id}, Name: {instance.name}")
 
 def delete_instance(conn, instance_name):
     try:
         instancia = conn.compute.find_server(instance_name)
+
         if instancia:
             conn.compute.delete_server(instancia, ignore_missing=True)
             print(f"Instance '{instance_name}' deletion success.")
+            print('')
+
+            delete_keypair(conn, instance_name)
         else:
             print(f"There is not an instance with that name '{instance_name}'.")
+            print('')
     except Exception as e:
         print(f"Error trying to delete the instance: {e}")
+        print('')
 
-# Initialize the connection
+def delete_keypair(conn, keypair_name):
+    try:
+        keypair = conn.compute.find_keypair(keypair_name)
+
+        if keypair:
+            conn.compute.delete_keypair(keypair)
+            print(f"Keypair '{keypair_name}' deletion success.")
+            print('')
+        else:
+            print(f"There is no keypair with the name '{keypair_name}'.")
+            print('')
+    except Exception as e:
+        print(f"Error trying to delete the keypair: {e}")
+        print('')
+
 conn = openstack.connect(cloud='openstack')
 
-# List and select an instance to delete
 list_instances(conn)
-instance_name_to_delete = input("Enter the name of the instance to delete: ")
+print('')
 
-# Delete the selected instance
+instance_name_to_delete = input("Enter the name of the instance to delete: ")
+print('')
+
 delete_instance(conn, instance_name_to_delete)
