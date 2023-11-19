161cea4635e30d2587a24e159ed4e6ba29e96297
Gaston Descalzo <gdescalzo@outlook.com>
2023-11-19 10:13:46 -0300

    Enhance the list module

--- OpenStackSDK/Instances/Instance_list.py
+++ OpenStackSDK/Instances/Instance_list.py
@@ -1,7 +1,13 @@
 import openstack
 
-# Initialize connection
+def list_instances(conn):
+    instances = conn.compute.servers()
+    print("Available instances:")
+    for instance in instances:
+        print(f"ID: {instance.id}, Name: {instance.name}")
+
+# Initialize the connection
 conn = openstack.connect(cloud='openstack')
 
-for server in conn.compute.servers():
-    print(server.to_dict())
+# List and select an instance to delete
+list_instances(conn)
