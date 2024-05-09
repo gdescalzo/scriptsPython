3dba1fd13d053a22d7feed3021a77e11559e8f47
Gaston Descalzo <77273777+gdescalzo@users.noreply.github.com>
2024-05-09 12:37:49 -0300

    Add files via upload

---
+++ AWS/SES/SES-E-Mails_healthy.py
@@ -0,0 +1,41 @@
+import boto3
+import json
+
+# Load the credentials from a JSON file
+def load_credentials(json_filepath):
+    with open(json_filepath, 'r') as f:
+        data = json.load(f)
+
+    credentials = data['credentials'][0]
+    access_key = credentials['access_key']
+    secret_access_key = credentials['secret_access_key']
+    region_name = credentials['region_name']
+
+    return access_key, secret_access_key, region_name
+
+# Use the function to load credentials:
+filepath = r'C:\Users\descalzo\OneDrive - Boehringer Ingelheim\Documents\Repos\scriptsPython\AWS\SES\Credentials.json'
+access_key, secret_access_key, region_name = load_credentials(filepath)
+
+# Initialize the AWS Health client
+client = boto3.client('health', 
+                      region_name="us-east-1", # should use the global endpoint, which is health.us-east-1.amazonaws.com
+                      aws_access_key_id=access_key, 
+                      aws_secret_access_key=secret_access_key)
+
+def check_ses_health():
+    # Get the AWS Health events
+    response = client.describe_events(filter={'services': ['SES']})
+    # Print the response
+    for event in response['events']:
+        print(f"Service: {event['service']}")
+        print(f"Event Type Code: {event['eventTypeCode']}")
+        print(f"Event Type Category: {event['eventTypeCategory']}")
+        print(f"Region: {event['region']}")
+        print(f"Start Time: {event['startTime']}")
+        print(f"Last Updated Time: {event['lastUpdatedTime']}")
+        print(f"Status Code: {event['statusCode']}")
+        print("\n")
+
+# Call the function to check the SES health
+check_ses_health()
---
+++ AWS/SES/SES-E-Mails_sent.py
@@ -0,0 +1,50 @@
+import boto3
+import json
+from datetime import datetime
+
+# Load the credentials from a JSON file
+def load_credentials(json_filepath):
+    with open(json_filepath, 'r') as f:
+        data = json.load(f)
+
+    credentials = data['credentials'][0]
+    access_key = credentials['access_key']
+    secret_access_key = credentials['secret_access_key']
+    region_name = credentials['region_name']
+
+    return access_key, secret_access_key, region_name
+
+# Use the function to load credentials:
+filepath = r'C:\Users\descalzo\OneDrive - Boehringer Ingelheim\Documents\Repos\scriptsPython\AWS\SES\Credentials.json'
+access_key, secret_access_key, region_name = load_credentials(filepath)
+
+# Initialize the Amazon SES client
+client = boto3.client('ses', 
+                      region_name=region_name, 
+                      aws_access_key_id=access_key, 
+                      aws_secret_access_key=secret_access_key)
+
+# Set the threshold for the number of emails
+threshold = 2500
+
+def check_email_sent():
+    # Get the current date
+    today = datetime.now().strftime('%Y-%m-%d')
+
+    # Get the send quota for the current date
+    response = client.get_send_quota()
+
+    # Get the number of emails sent today
+    emails_sent_today = response['SentLast24Hours']
+
+    # Check if the number of emails sent is greater than the threshold
+    if emails_sent_today > threshold:
+        print(f'Warning: Email threshold exceeded. {emails_sent_today} emails sent on {today}.')
+    else:
+        # Print the response
+        print(f"Max 24 Hour Send: {response['Max24HourSend']}")
+        print(f"Max Send Rate: {response['MaxSendRate']}")
+        print(f"Sent Last 24 Hours: {response['SentLast24Hours']}")
+
+# Call the function to check the number of emails sent
+check_email_sent()
